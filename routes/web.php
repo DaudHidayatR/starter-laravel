26aab8dac98048f118e105760f87e0528fa2df16
DaudHidayatRamadhn <daud28ramadhan@gmail.com>
2023-06-01 11:42:50 +0700

    menambahkan fortify

--- routes/web.php
+++ routes/web.php
@@ -16,19 +16,19 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('dashboards.home');
 });
-Route::get('/baru', function () {
-    return view('dashboards.baru');
-});
-Route::get('/login', function () {
-    return view('auth.login');
-})->name('login');
-Route::get('/register', function () {
-    return view('auth.register');
-})->name('register');
-
-Route::get('/forgot', function () {
-    return view('auth.forgot');
-})->name('forgot');
-Route::get('/reset', function () {
-    return view('auth.reset');
-})->name('reset');
+//Route::get('/baru', function () {
+//    return view('dashboards.baru');
+//});
+//Route::get('/login', function () {
+//    return view('auth.login');
+//})->name('login');
+//Route::get('/register', function () {
+//    return view('auth.register');
+//})->name('register');
+//
+//Route::get('/forgot', function () {
+//    return view('auth.forgot');
+//})->name('forgot');
+//Route::get('/reset', function () {
+//    return view('auth.reset');
+//})->name('reset');
